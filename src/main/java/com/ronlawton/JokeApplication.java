aea593d29ec21a737930460717f74f3c3fa8352d
rlawton2 <rlawton1234@gmail.com>
2018-08-23 21:20:47 -0700

    initial commit

---
+++ src/main/java/com/ronlawton/JokeApplication.java
@@ -0,0 +1,12 @@
+package com.ronlawton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeApplication.class, args);
+    }
+}
---
+++ src/main/java/com/ronlawton/service/JokeService.java
@@ -0,0 +1,5 @@
+package com.ronlawton.service;
+
+public interface JokeService {
+    String getJoke();
+}
